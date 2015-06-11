d7b904166af704a446b57a534b756832ea53e2a9
Vincent Slater <vincent1slater@gmail.com>
2015-06-11 19:55:25 +0100

    Commit Gui.h

---
+++ Movies/Gui.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Database.h";
+
+class Gui
+{
+public:
+	Gui(Database db);
+	~Gui();
+private:
+	Database myDb;
+	void listFilmsByActor();
+	void listFilmInfo();
+	void addNewFilm();
+};
+
